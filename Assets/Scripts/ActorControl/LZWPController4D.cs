70b52f28f1d9215e7fa667e2c117ed23cf79bb3d
Jakub Stasiorowski <kuba.stasiorowski@wp.pl>
2022-11-26 11:35:07 +0100

    Swap joystick axes

--- Assets/Scripts/ActorControl/LZWPController4D.cs
+++ Assets/Scripts/ActorControl/LZWPController4D.cs
@@ -6,10 +6,10 @@ using System.Threading.Tasks;
 
 class LZWPController4D : IInput4D
 {
-    public float GetXAxis() => Lzwp.input.flysticks[0].joysticks[1]; // 1st joystick horizontal axis
-    public float GetYAxis() => Lzwp.input.flysticks[1].joysticks[0]; // 2nd joystick vertical axis
-    public float GetZAxis() => Lzwp.input.flysticks[0].joysticks[0]; // 1st joystick vertical axis
-    public float GetWAxis() => Lzwp.input.flysticks[1].joysticks[1]; // 2nd joystick horizontal axis
+    public float GetXAxis() => Lzwp.input.flysticks[0].joysticks[0]; // 1st joystick horizontal axis
+    public float GetYAxis() => Lzwp.input.flysticks[1].joysticks[1]; // 2nd joystick vertical axis
+    public float GetZAxis() => Lzwp.input.flysticks[0].joysticks[1]; // 1st joystick vertical axis
+    public float GetWAxis() => Lzwp.input.flysticks[1].joysticks[0]; // 2nd joystick horizontal axis
 
     public float GetXYRotation() => GetButtonAxis1();
     public float GetXZRotation() => GetButtonAxis2();
