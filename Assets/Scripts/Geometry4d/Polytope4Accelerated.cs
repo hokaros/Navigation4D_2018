d538c5b6cb4c960542cac1f6703808682be68f33
Jakub Stasiorowski <kuba.stasiorowski@wp.pl>
2022-11-27 22:09:36 +0100

    Use compute shader to intersect edges with a hyperplane

--- Assets/Scripts/Geometry4d/Polytope4Accelerated.cs
+++ Assets/Scripts/Geometry4d/Polytope4Accelerated.cs
@@ -11,6 +11,7 @@ using UnityEngine;
 public class Polytope4Accelerated : Polytope4
 {
     [SerializeField] ComputeShader meshCalculationsShader;
+    [SerializeField] ComputeShader edgeIntersectionShader;
 
     protected override Dictionary<int, List<int>> GetFacesForEdgesUncached()
     {
@@ -63,6 +64,95 @@ public class Polytope4Accelerated : Polytope4
         return GetFacesForEdges(edgeTriangles, (triangleIndex) => triangleToFace[triangleIndex]);
     }
 
+    /// <summary>
+    /// Intersects hyperplane with faces of the polytope
+    /// </summary>
+    /// <param name="hyperplane"></param>
+    /// <returns>A dictionary where: Key = index of the face; Value = intersection points with the edges of the face. </returns>
+    public override Dictionary<int, List<Vector4>> FacesIntersections(Hyperplane4 hyperplane)
+    {
+        List<Vector4> vertices = VerticesWorld;
+
+        Dictionary<int, List<Vector4>> faceIntersections = new Dictionary<int, List<Vector4>>(); // faceIndex -> intersection points
+
+        int kernelIndex = edgeIntersectionShader.FindKernel("IntersectEdges");
+        // Load data into the shader
+        ComputeBuffer vertexBuffer = new ComputeBuffer(vertices.Count, sizeof(float) * 4);
+        vertexBuffer.SetData(vertices);
+        edgeIntersectionShader.SetBuffer(kernelIndex, "vertices", vertexBuffer);
+        edgeIntersectionShader.SetInt("vertexCount", vertices.Count);
+
+        Vector2Int[] edgesArray = edges.Select(e => new Vector2Int(e.startId, e.endId)).ToArray();
+        ComputeBuffer edgeBuffer = new ComputeBuffer(edgesArray.Length, sizeof(int) * 2);
+        edgeBuffer.SetData(edgesArray);
+        edgeIntersectionShader.SetBuffer(kernelIndex, "edges", edgeBuffer);
+        edgeIntersectionShader.SetInt("edgeCount", edgesArray.Length);
+
+        // Load hyperplane
+        edgeIntersectionShader.SetFloat("hyperplaneA", hyperplane.A);
+        edgeIntersectionShader.SetFloat("hyperplaneB", hyperplane.B);
+        edgeIntersectionShader.SetFloat("hyperplaneC", hyperplane.C);
+        edgeIntersectionShader.SetFloat("hyperplaneD", hyperplane.D);
+        edgeIntersectionShader.SetFloat("hyperplaneE", hyperplane.E);
+
+        // Result buffer
+        IntersectionInfo[] intersectionsArray = new IntersectionInfo[edgesArray.Length];
+        ComputeBuffer intersectionData = new ComputeBuffer(intersectionsArray.Length, sizeof(int) + sizeof(float) * 4);
+        edgeIntersectionShader.SetBuffer(kernelIndex, "intersections", intersectionData);
+        // Init result count as 0
+        int[] intersectionCountArray = new int[1];
+        intersectionCountArray[0] = 0;
+        ComputeBuffer intersectionCountBuffer = new ComputeBuffer(intersectionCountArray.Length, sizeof(int));
+        intersectionCountBuffer.SetData(intersectionCountArray);
+        edgeIntersectionShader.SetBuffer(kernelIndex, "intersectionCount", intersectionCountBuffer);
+
+        // Execute shader
+        uint threadGroupSizeX;
+        uint threadGroupSizeY;
+        uint threadGroupSizeZ;
+        edgeIntersectionShader.GetKernelThreadGroupSizes(kernelIndex, out threadGroupSizeX, out threadGroupSizeY, out threadGroupSizeZ);
+        edgeIntersectionShader.Dispatch(kernelIndex, (int)Mathf.Ceil(edgesArray.Length / (float)threadGroupSizeX), 1, 1);
+
+        // Get edge intersections from the shader
+        intersectionData.GetData(intersectionsArray);
+        intersectionCountBuffer.GetData(intersectionCountArray);
+
+        // Clean up
+        vertexBuffer.Dispose();
+        edgeBuffer.Dispose();
+        intersectionData.Dispose();
+        intersectionCountBuffer.Dispose();
+
+        return EdgeIntersectionsToFaceIntersections(intersectionsArray, intersectionCountArray[0]);
+    }
+
+    private Dictionary<int, List<Vector4>> EdgeIntersectionsToFaceIntersections(IntersectionInfo[] intersections, int intersectionCount)
+    {
+        Dictionary<int, List<Vector4>> faceIntersections = new Dictionary<int, List<Vector4>>();
+
+        for(int i = 0; i < intersectionCount; i++)
+        {
+            int edgeIndex = intersections[i].edgeIndex;
+            Vector4 intersectionPoint = intersections[i].intersectionPoint;
+
+            // Save the face which the edge belongs to, clone record for every face
+            foreach (int faceIndex in GetEdgeFaces(edgeIndex))
+            {
+                // Add the intersection to the face's record
+                if (faceIntersections.ContainsKey(faceIndex))
+                {
+                    faceIntersections[faceIndex].Add(intersectionPoint);
+                }
+                else
+                {
+                    faceIntersections.Add(faceIndex, new List<Vector4> { intersectionPoint });
+                }
+            }
+        }
+
+        return faceIntersections;
+    }
+
     private Dictionary<int, List<int>> GetFacesForEdges(Vector3Int[] edgeTriangles, Func<int, int> triangleToFace)
     {
         // Wprowadzenie danych do słownika
@@ -91,12 +181,19 @@ public class Polytope4Accelerated : Polytope4
         return facesForEdges;
     }
 
+
     public static IEnumerable<Vector3Int> FaceToTriangles(List<int> faceVertices)
     {
         for (int i = 2; i < faceVertices.Count; i++)
         {
             yield return new Vector3Int(faceVertices[i - 1], faceVertices[i], faceVertices[0]); // triangle fan
         }
+    }
+
+    private struct IntersectionInfo
+    {
+        public int edgeIndex; // Index of the intersected edge
+        public Vector4 intersectionPoint;
     }
 }
 
