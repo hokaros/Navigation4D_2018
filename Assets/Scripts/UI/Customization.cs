e930e40031cea5210d067f1362467959a97b4757
Adam Leczkowski <58361333+nne-nne@users.noreply.github.com>
2022-11-30 18:29:14 +0100

    position and rotation buttons in panel

--- Assets/Scripts/UI/Customization.cs
+++ Assets/Scripts/UI/Customization.cs
@@ -7,14 +7,33 @@ public class Customization : MonoBehaviour
 {
     [SerializeField] Text title;
 
+    [SerializeField] private GameObject positionPanel;
+    [SerializeField] private GameObject rotationPanel;
+
+
     [SerializeField] private GameObject activeGameObject;
     private Polytope4 activePolytope;
     private Transform4 activeTransform;
+
+
+    private void DeactivateOptions()
+    {
+        positionPanel.SetActive(false);
+        rotationPanel.SetActive(false);
+    }
+
+    private void ActivatePanel(GameObject panel)
+    {
+        DeactivateOptions();
+        panel.SetActive(true);
+    }
+
 
     public void SetPolytope(GameObject polytope)
     {
         activeGameObject = polytope;
         activePolytope = polytope.GetComponent<Polytope4>();
+        activeTransform = activePolytope.transform4;
         title.text = activePolytope.name;
     }
 
@@ -24,6 +43,16 @@ public class Customization : MonoBehaviour
         {
             SetPolytope(activeGameObject);
         }
+    }
+
+    public void OnPositionButtonClicked()
+    {
+        ActivatePanel(positionPanel);
+    }
+
+    public void OnRotationButtonClicked()
+    {
+        ActivatePanel(rotationPanel);
     }
 
     void Update()
