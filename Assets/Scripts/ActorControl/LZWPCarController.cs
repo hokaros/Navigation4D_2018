767290263222c11077644223b2d0c30a701483ec
Jakub Stasiorowski <kuba.stasiorowski@wp.pl>
2022-12-06 16:53:20 +0100

    Car controller

---
+++ Assets/Scripts/ActorControl/LZWPCarController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+class LZWPCarController : LZWPControllerBase, IInput4D
+{
+    public float GetZAxis() => GetButtonAxisFire();
+
+    // Disable the rest of movements
+    public float GetXAxis() => 0f;
+    public float GetYAxis() => 0f;
+    public float GetWAxis() => 0f;
+
+    public float GetXYRotation() => Lzwp.input.flysticks[0].joysticks[0]; // 1st joystick horizontal axis
+
+    public float GetXZRotation() => Lzwp.input.flysticks[1].joysticks[0]; // 2nd joystick horizontal axis
+
+    public float GetXWRotation() => GetButtonAxis1();
+
+    public float GetYZRotation() => Lzwp.input.flysticks[0].joysticks[1]; // 1st joystick vertical axis 
+
+    public float GetYWRotation() => Lzwp.input.flysticks[1].joysticks[1]; // 2nd joystick vertical axis
+
+    public float GetZWRotation() => GetButtonAxis3();
+
+
+    public bool TriggerRaycast()
+    {
+        throw new NotImplementedException();
+    }
+
+    RaycastHit[] IInput4D.RaycastClick()
+    {
+        return base.RaycastClick();
+    }
+}
---
+++ Assets/Scripts/ActorControl/LZWPControllerBase.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+class LZWPControllerBase
+{
+    protected float GetButtonAxis1()
+    {
+        LzwpInput.Button btnPositive = Lzwp.input.flysticks[0].GetButton(LzwpInput.Flystick.ButtonID.Button1);
+        LzwpInput.Button btnNegative = Lzwp.input.flysticks[0].GetButton(LzwpInput.Flystick.ButtonID.Button2);
+        return GetButtonAxis(btnPositive, btnNegative);
+    }
+
+    protected float GetButtonAxis2()
+    {
+        LzwpInput.Button btnPositive = Lzwp.input.flysticks[0].GetButton(LzwpInput.Flystick.ButtonID.Button3);
+        LzwpInput.Button btnNegative = Lzwp.input.flysticks[0].GetButton(LzwpInput.Flystick.ButtonID.Button4);
+        return GetButtonAxis(btnPositive, btnNegative);
+    }
+    protected float GetButtonAxis3()
+    {
+        LzwpInput.Button btnPositive = Lzwp.input.flysticks[1].GetButton(LzwpInput.Flystick.ButtonID.Button1);
+        LzwpInput.Button btnNegative = Lzwp.input.flysticks[1].GetButton(LzwpInput.Flystick.ButtonID.Button2);
+        return GetButtonAxis(btnPositive, btnNegative);
+    }
+    protected float GetButtonAxis4()
+    {
+        LzwpInput.Button btnPositive = Lzwp.input.flysticks[1].GetButton(LzwpInput.Flystick.ButtonID.Button3);
+        LzwpInput.Button btnNegative = Lzwp.input.flysticks[1].GetButton(LzwpInput.Flystick.ButtonID.Button4);
+        return GetButtonAxis(btnPositive, btnNegative);
+    }
+
+    protected float GetButtonAxisFire()
+    {
+        LzwpInput.Button btnPositive = Lzwp.input.flysticks[0].GetButton(LzwpInput.Flystick.ButtonID.Fire);
+        LzwpInput.Button btnNegative = Lzwp.input.flysticks[1].GetButton(LzwpInput.Flystick.ButtonID.Fire);
+        return GetButtonAxis(btnPositive, btnNegative);
+    }
+
+    protected float GetButtonAxisJoystick()
+    {
+        LzwpInput.Button btnPositive = Lzwp.input.flysticks[0].GetButton(LzwpInput.Flystick.ButtonID.Joystick);
+        LzwpInput.Button btnNegative = Lzwp.input.flysticks[1].GetButton(LzwpInput.Flystick.ButtonID.Joystick);
+        return GetButtonAxis(btnPositive, btnNegative);
+    }
+
+    protected float GetButtonAxis(LzwpInput.Button btnPositive, LzwpInput.Button btnNegative)
+    {
+        float positive = btnPositive.isActive ? 1f : 0f;
+        float negative = btnNegative.isActive ? 1f : 0f;
+
+        return positive - negative;
+    }
+
+    protected RaycastHit[] RaycastClick()
+    {
+        Vector3 origin = Vector3.forward; // retrieve flystick position
+        // TODO: implement
+        return Physics.RaycastAll(Camera.main.ScreenPointToRay(origin));
+    }
+}
